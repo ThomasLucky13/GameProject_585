7285b58ad1e4fd8531576595e0e2550492cdb263
Thomas Cole <thomas_lucky13@mail.ru>
2021-09-30 15:37:22 +0400

    После выгрузки в unity

---
+++ lib/Maze_Generate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ellers_Algorithm;
+using n_Cell;
+
+
+namespace n_Maze
+{
+    public class Maze
+    {
+        public static Cell[,] Get_map(int height, int width)
+        {
+            Eller_Generation maze = new Eller_Generation();
+            maze.Generate(height, width);
+            return maze.Maze();
+        }
+    }
+}
